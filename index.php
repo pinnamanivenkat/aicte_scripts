4f87e6c06e27df3788867c56f2adc675d4edaab0
pinnamanivenkat <pinnamanivenkat@gmail.com>
2017-01-19 11:08:50 +0530

    Script to disable right click

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title> Sample </title>
+</head>
+<body>
+<!-- Right click disabled-->
+	<script>
+		document.addEventListener('contextmenu', event => event.preventDefault());
+	</script>
+<!-- Right click disabled end -->
+<p> Right click disabled</p>
+</body>
+</html>
